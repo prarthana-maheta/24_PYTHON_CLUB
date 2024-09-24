fb6b376583e264adf979953876900a0dcf47c618
prarthana-maheta <142563768+prarthana-maheta@users.noreply.github.com>
2024-09-24 21:17:00 +0530

    Add files via upload

--- sep_24.py
+++ sep_24.py
@@ -89,3 +89,52 @@ print(dict1)
 #         if '.' in v:
 
         # isinstance(v,str)
+
+# Write a Python program to convert a dictionary into a list of lists.
+# Original Dictionary:
+# {1: 'red', 2: 'green', 3: 'black', 4: 'white', 5: 'black'}
+# Convert the said dictionary into a list of lists:
+# [[1, 'red'], [2, 'green'], [3, 'black'], [4, 'white'], [5, 'black']]
+# Original Dictionary:
+# {'1': 'Austin Little', '2': 'Natasha Howard', '3': 'Alfred Mullins', '4': 'Jamie Rowe'}
+# Convert the said dictionary into a list of lists:
+# [['1', 'Austin Little'], ['2', 'Natasha Howard'], ['3', 'Alfred Mullins'], ['4', 'Jamie Rowe']]
+
+# Write a Python program to combine 
+# two lists into a dictionary.
+# The elements of the first one serve as keys and the elements of the second
+# one serve as values. Each item in the first list must be unique and hashable.
+# Sample Output:
+# Original lists:
+# a=['a', 'b', 'c', 'd', 'e', 'f']
+# b=[1, 2, 3, 4, 5,8,8,9]
+# # Combine the values of the said two lists into a dictionary:
+# # {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+
+# print(dict(zip(a,b)))
+
+
+# function 
+
+# 2 inbuilt,
+#  user defined 
+
+# parameterized function 
+# non paratermized function
+# default parameterized function 
+
+# keyword arguments
+# * args- arbitary args
+# ** kwargs - keyword arbitary args
+# def func1(name,age,height,weight):
+
+#     pass
+# func1(age=10,heigh=12,weight=12,name='royal')
+
+# def func1(name,*args,**args1):
+#     print(args,args1,nam)
+
+# func1(1,2,3,4,5,6,7,8,9,0,name='royal',age=12)
+
+
+
